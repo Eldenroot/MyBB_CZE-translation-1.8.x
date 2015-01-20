58ea7d7ddd6eaa9260a61dc14cf07924b54d3685
petrzk <petulmail@gmail.com>
2015-01-20 18:26:14 +0100

    Update datahandler_pm.lang.php

--- czech/datahandler_pm.lang.php
+++ czech/datahandler_pm.lang.php
@@ -12,10 +12,10 @@ $l['pmdata_missing_message'] = 'Chybí obsah zprávy. Prosím, doplňte jej.';
 $l['pmdata_invalid_recipients'] = "Na seznamu příjemců zprávy se nachází příjemce, nebo příjemci, které neexistují v seznamu uživatelů. Chybní příjemci: {1}.";
 $l['pmdata_no_recipients'] = "Chybí uvedení příjemce zprávy. Musíte zadat alespoň jednoho příjemce.";
 $l['pmdata_too_many_recipients'] = "Nemáte povolení naráz poslat zprávu {1} uživatelům.";
-$l['pmdata_recipient_is_ignoring'] = "Osoba, které se pokoušíte zaslat soukromou zprávu, Vás má umístěného v seznamu ignorovaných uživatelů. Vaše zpráva jí tedy nemůže být odeslána.";
+$l['pmdata_recipient_is_ignoring'] = "Uživatel, kterému se pokoušíte zaslat soukromou zprávu, Vás má umístěného v seznamu ignorovaných uživatelů. Vaše zpráva mu tedy nemůže být odeslána.";
 $l['pmdata_recipient_has_buddy_only'] = "Nemůžete poslat zprávu příjemci {1}, protože Vás nemá ve svém seznamu přátel.";
-$l['pmdata_recipient_pms_disabled'] = 'Osoba {1}, které se pokoušíte zaslat soukromou zprávu, si tuto možnost vypnula. Vaše zpráva jí tedy nemůže být odeslána.';
-$l['pmdata_recipient_reached_quota'] = 'Osoba {1}, které se pokoušíte zaslat soukromou zprávu, má zcela plnou schránku pro jejich příjem. Vaše zpráva jí tedy nemůže být odeslána.';
+$l['pmdata_recipient_pms_disabled'] = 'Uživatel {1}, kterému se pokoušíte zaslat soukromou zprávu, si tuto možnost vypnula. Vaše zpráva mu tedy nemůže být odeslána.';
+$l['pmdata_recipient_reached_quota'] = 'Uživatel {1}, kterému se pokoušíte zaslat soukromou zprávu, má zcela plnou schránku pro jejich příjem. Vaše zpráva mu tedy nemůže být odeslána.';
 
 $l['pmdata_pm_flooding'] = 'Pokoušíte se odeslat zprávu příliš brzy po předchozí. Prosím, počkejte ještě další {1} sekund.';
 $l['pmdata_pm_flooding_one_second'] = 'Pokoušíte se odeslat zprávu moc brzy po odeslání předchozí zprávy. Prosím počkejte 1 sekundu.';
