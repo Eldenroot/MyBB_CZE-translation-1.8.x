639fa55bbb75bd399249bb7e5eedbb9704b506a7
Josef Schlehofer <github@bloodkings.eu>
2017-07-23 18:48:16 +0200

    korekce

--- czech/admin/home_credits.lang.php
+++ czech/admin/home_credits.lang.php
@@ -9,6 +9,6 @@ $l['mybb_credits'] = "Autoři systému MyBB";
 $l['mybb_credits_description'] = "Tito lidé věnovali svůj čas a snahu vytvořit MyBB.";
 $l['about_the_team'] = "O týmu";
 $l['check_for_updates'] = "Zkontrolovat aktualizace autorů MyBB";
-$l['error_communication'] = "Nastal zde problém se stažením novějších zásluh členů MyBB. Vydrž, prosím, chvíli a zkus to znova.";
+$l['error_communication'] = "Nastal zde problém se stažením novějších členů MyBB, případně aktualizací údajů. Vydrž, prosím, chvíli a zkus to znova.";
 $l['no_credits'] = "Žádné uložené informace ohledně autorů a vyvojářů MyBB. <a href=\"index.php?module=home-credits&amp;fetch_new=1\">Načíst údaje</a>.";
 $l['success_credits_updated'] = 'Mezipaměť zásluh autorům MyBB byla úspěšně aktualizována.';
