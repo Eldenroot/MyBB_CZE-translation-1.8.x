89a6d913c77f3aba781b6c7afb5150644a67fd92
Josef Schlehofer <github@bloodkings.eu>
2017-07-23 17:32:42 +0200

    Administrace - 1%

---
+++ czech/admin/tools_php_info.lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * MyBB 1.8.12 Czech Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['php_info'] = "PHP Info";
+$l['browser_no_iframe_support'] = "Your browser does not support iFrames.";
+
---
+++ czech/admin/user_module_meta.lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MyBB 1.8.12 Czech Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['users_and_groups'] = "Uživatelé &amp; skupiny";
+
+$l['users'] = "Uživatelé";
+$l['awaiting_activation'] = "Čekající na aktivaci";
+$l['groups'] = "Skupiny";
+$l['user_titles'] = "User Titles";
+$l['banning'] = "Banning";
+$l['admin_permissions'] = "Práva administrátora";
+$l['mass_mail'] = "Hromadný email (newsletter)";
+$l['group_promotions'] = "Group Promotions";
+
+$l['can_manage_users'] = "Can manage users?";
+$l['can_manage_awaiting_activation'] = "Can manage awaiting activation?";
+$l['can_manage_user_groups'] = "Can manage user groups?";
+$l['can_manage_user_titles'] = "Can manage user titles?";
+$l['can_manage_user_bans'] = "Can manage User Bans?";
+$l['can_manage_admin_permissions'] = "Can manage admin permissions?";
+$l['can_send_mass_mail'] = "Can send mass mail?";
+$l['can_manage_group_promotions'] = "Can manage group promotions?";
