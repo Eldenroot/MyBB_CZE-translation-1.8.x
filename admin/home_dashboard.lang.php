eb7a57594713f10d77562311ac0a98befcb2433d
Josef Schlehofer <pepe.schlehofer@gmail.com>
2017-07-23 16:49:01 +0200

    Upload souborů z anglické administrace 1.8.12

---
+++ admin/home_dashboard.lang.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['dashboard'] = "Nástěnka";
+$l['dashboard_description'] = "This section allows you to see some of the various statistics relating to your board. You may also add other notes for other administrators to see.";
+
+$l['mybb_server_stats'] = "MyBB a statistiky serveru";
+$l['forum_stats'] = "Statistiky fóra";
+$l['mybb_version'] = "MyBB verze";
+$l['threads'] = "Threads";
+$l['new_today'] = "New Today";
+$l['unapproved'] = "Unapproved";
+$l['php_version'] = "PHP verze";
+$l['posts'] = "Příspěvky";
+$l['sql_engine'] = "SQL Engine";
+$l['users'] = "Users";
+$l['registered_users'] = "Registrovaní uživatelé";
+$l['active_users'] = "Aktivní uživatelé";
+$l['registrations_today'] = "Dnešní registrace";
+$l['awaiting_activation'] = "Awaiting Activation";
+$l['server_load'] = "Zatížení serveru";
+$l['attachments'] = "Přílohy";
+$l['used'] = "Used";
+$l['reported_posts'] = "Reported Posts";
+$l['unread_reports'] = "Unread Reports";
+
+$l['version_check'] = "Zkontrolovat aktualizace";
+$l['last_update_check_two_weeks'] = "Your last <a href=\"{1}\">MyBB version check</a> was more than two weeks ago.";
+$l['new_version_available'] = "You are currently running {1} whilst the latest generally available release is {2}.";
+$l['version_check_description'] = "Here you can check that you are currently running the latest copy of MyBB and see the latest announcements directly from MyBB.";
+$l['latest_mybb_announcements'] = "Latest MyBB Announcements";
+$l['no_announcements'] = "No stored announcements. <a href=\"index.php?module=home&amp;action=version_check\">Check for Updates</a>.";
+$l['your_version'] = "Tvá verze";
+$l['latest_version'] = "Nejaktuálnější verze MyBB";
+$l['update_forum'] = "Please upgrade to the latest version of MyBB by visiting the <a href=\"https://mybb.com\" target=\"_blank\">MyBB Website</a>.";
+$l['read_more'] = "Read more";
+
+$l['success_up_to_date'] = "Gratuluji! Používáš aktuální verzi MyBB.";
+
+$l['error_out_of_date'] = "Tvé verze MyBB, kterou používáš je zastaralá. Doporučuji aktualizovat na nejnovější verzi.";
+$l['error_communication'] = "There was a problem communicating with the version server. Please try again in a few minutes.";
+$l['error_fetch_news'] = "MyBB was unable to successfully fetch the latest announcements from the MyBB website.";
+
+$l['news_description'] = "The latest news from the <a href=\"https://blog.mybb.com/\" target=\"_blank\">MyBB Blog</a>.";
+
+$l['admin_notes_public'] = "These notes are public to all administrators.";
+$l['admin_notes'] = "Administrator Notes";
+$l['save_notes'] = "Save Notes";
+
+$l['success_notes_updated'] = "The administrator notes have been successfully updated.";
