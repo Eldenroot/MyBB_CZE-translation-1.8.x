f6907727964d94c1dcc03ed16e35b2fc6d4d3bbb
Josef Schlehofer <github@bloodkings.eu>
2017-07-23 17:54:09 +0200

    90%

--- czech/admin/home_dashboard.lang.php
+++ czech/admin/home_dashboard.lang.php
@@ -6,49 +6,49 @@
  */
 
 $l['dashboard'] = "Nástěnka";
-$l['dashboard_description'] = "This section allows you to see some of the various statistics relating to your board. You may also add other notes for other administrators to see.";
+$l['dashboard_description'] = "Tato sekce ti ukáže statistiky tvého fóra. Také zde můžeš přidat poznámku pro ostatní administrátory.";
 
 $l['mybb_server_stats'] = "MyBB a statistiky serveru";
 $l['forum_stats'] = "Statistiky fóra";
 $l['mybb_version'] = "MyBB verze";
-$l['threads'] = "Threads";
-$l['new_today'] = "New Today";
-$l['unapproved'] = "Unapproved";
+$l['threads'] = "Témata";
+$l['new_today'] = "Dnes založená";
+$l['unapproved'] = "Neschvalená";
 $l['php_version'] = "PHP verze";
 $l['posts'] = "Příspěvky";
 $l['sql_engine'] = "SQL Engine";
-$l['users'] = "Users";
+$l['users'] = "Uživatelé";
 $l['registered_users'] = "Registrovaní uživatelé";
 $l['active_users'] = "Aktivní uživatelé";
 $l['registrations_today'] = "Dnešní registrace";
-$l['awaiting_activation'] = "Awaiting Activation";
+$l['awaiting_activation'] = "Čekající na aktivaci";
 $l['server_load'] = "Zatížení serveru";
 $l['attachments'] = "Přílohy";
 $l['used'] = "Used";
-$l['reported_posts'] = "Reported Posts";
-$l['unread_reports'] = "Unread Reports";
+$l['reported_posts'] = "Nahlášené příspěvky";
+$l['unread_reports'] = "Nepřečtené reportys";
 
 $l['version_check'] = "Zkontrolovat aktualizace";
-$l['last_update_check_two_weeks'] = "Your last <a href=\"{1}\">MyBB version check</a> was more than two weeks ago.";
+$l['last_update_check_two_weeks'] = "Naposledy si <a href=\"{1}\">zkontroloval verzi MyBB</a> před více než 2 týdny!";
 $l['new_version_available'] = "You are currently running {1} whilst the latest generally available release is {2}.";
 $l['version_check_description'] = "Here you can check that you are currently running the latest copy of MyBB and see the latest announcements directly from MyBB.";
-$l['latest_mybb_announcements'] = "Latest MyBB Announcements";
+$l['latest_mybb_announcements'] = "Nejnovější oznámení MyBB";
 $l['no_announcements'] = "No stored announcements. <a href=\"index.php?module=home&amp;action=version_check\">Check for Updates</a>.";
 $l['your_version'] = "Tvá verze";
 $l['latest_version'] = "Nejaktuálnější verze MyBB";
-$l['update_forum'] = "Please upgrade to the latest version of MyBB by visiting the <a href=\"https://mybb.com\" target=\"_blank\">MyBB Website</a>.";
-$l['read_more'] = "Read more";
+$l['update_forum'] = "Prosím aktualizuj své fórum na novější verzi <a href=\"https://mybb.com\" target=\"_blank\">MyBB</a>. Předejdeš problémům, které se mohou vyskytnout";
+$l['read_more'] = "Přečti si víc";
 
 $l['success_up_to_date'] = "Gratuluji! Používáš aktuální verzi MyBB.";
 
 $l['error_out_of_date'] = "Tvé verze MyBB, kterou používáš je zastaralá. Doporučuji aktualizovat na nejnovější verzi.";
-$l['error_communication'] = "There was a problem communicating with the version server. Please try again in a few minutes.";
-$l['error_fetch_news'] = "MyBB was unable to successfully fetch the latest announcements from the MyBB website.";
+$l['error_communication'] = "Při pokusu o spojení se serverem, který má na starost aktualizace nastal problém. Prosím, zkus to za chvíli. Snad už se to podaří! :)";
+$l['error_fetch_news'] = "Bohužel se nepodařilo nahrát získat novější oznámení ze stránek MyBB.";
 
-$l['news_description'] = "The latest news from the <a href=\"https://blog.mybb.com/\" target=\"_blank\">MyBB Blog</a>.";
+$l['news_description'] = "Nejnovější zprávy z <a href=\"https://blog.mybb.com/\" target=\"_blank\">MyBB blogu (v angličtině)</a>.";
 
-$l['admin_notes_public'] = "These notes are public to all administrators.";
-$l['admin_notes'] = "Administrator Notes";
-$l['save_notes'] = "Save Notes";
+$l['admin_notes_public'] = "Tyto poznámky jsou dostupné pro všechny administrátory";
+$l['admin_notes'] = "Administrátorské poznámky";
+$l['save_notes'] = "Uložit poznámky";
 
-$l['success_notes_updated'] = "The administrator notes have been successfully updated.";
+$l['success_notes_updated'] = "Administrátorské poznámky byly úspěšně aktualizovány.";
