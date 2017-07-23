e5064c40bc021b6f767f8d46167d9ffaee7adcfc
Josef Schlehofer <github@bloodkings.eu>
2017-07-24 01:23:33 +0200

    100% php_info

--- czech/admin/tools_php_info.lang.php
+++ czech/admin/tools_php_info.lang.php
@@ -6,5 +6,5 @@
  */
 
 $l['php_info'] = "PHP Info";
-$l['browser_no_iframe_support'] = "Your browser does not support iFrames.";
+$l['browser_no_iframe_support'] = "Tvůj prohložeč nepodporuje iFrames.";
 
