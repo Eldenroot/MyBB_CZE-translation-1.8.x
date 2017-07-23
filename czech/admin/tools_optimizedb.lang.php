63169f09d05b6e67f9d8cb288871c5a50916249d
Josef Schlehofer <github@bloodkings.eu>
2017-07-24 01:34:08 +0200

    90%  optimedb
    optimalizovány a analyzovány?

--- czech/admin/tools_optimizedb.lang.php
+++ czech/admin/tools_optimizedb.lang.php
@@ -5,16 +5,16 @@
  *
  */
 
-$l['optimize_database'] = "Optimize Database";
+$l['optimize_database'] = "Optimalizování databáze";
 
-$l['table_selection'] = "Table Selection";
-$l['tables_select_desc'] = "You may select the database tables you wish to perform this action on here. Hold down CTRL to select multiple tables.";
-$l['select_all'] = "Select All";
-$l['deselect_all'] = "Deselect All";
-$l['select_forum_tables'] = "Select Forum Tables";
-$l['optimize_selected_tables'] = "Optimize Selected Tables";
+$l['table_selection'] = "Zvolení tabulky";
+$l['tables_select_desc'] = "Můžeš si zvolit databázové tabulky na kterých chceš provést tuto akci. Podržením tlačítka CTRL můžeš vybrat více tabulek najednou.";
+$l['select_all'] = "Označit všechny";
+$l['deselect_all'] = "Odznačit vše";
+$l['select_forum_tables'] = "Označit všechny tabulky fóras";
+$l['optimize_selected_tables'] = "Optimalizovat vybraný výběr tabulek";
 
-$l['error_no_tables_selected'] = "You did not select any database tables to optimize.";
+$l['error_no_tables_selected'] = "Nevybral si žádnou databázovou tabulku k optimalizování.";
 
-$l['success_tables_optimized'] = "The selected tables have been optimized and analyzed successfully.";
+$l['success_tables_optimized'] = "Vybrané tabulky byly optimalizovány a analyzed úspěšně.";
 
