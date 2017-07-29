720cae749568ff163f0dd98325f85c017b5ac33a
Josef Schlehofer <github@bloodkings.eu>
2017-07-29 19:40:22 +0200

    Smajlíci - většina přeložena

--- czech/admin/config_smilies.lang.php
+++ czech/admin/config_smilies.lang.php
@@ -5,60 +5,60 @@
  *
  */
 
-$l['smilies'] = "Smilies";
-$l['manage_smilies'] = "Manage Smilies";
-$l['manage_smilies_desc'] = "This section allows you to edit and delete your smilies.";
-$l['add_smilie'] = "Add New Smilie";
-$l['add_smilie_desc'] = "Here you can add a single new smilie.";
-$l['add_multiple_smilies'] = "Add Multiple Smilies";
-$l['add_multiple_smilies_desc'] = "Here you can add multiple new smilies at once.";
-$l['edit_smilie'] = "Edit Smilie";
-$l['edit_smilie_desc'] = "Here you can edit a single smilie.";
-$l['mass_edit'] = "Mass Edit";
-$l['mass_edit_desc'] = "Here you can easily edit all your smilies in one go.";
+$l['smilies'] = "Smajlíci";
+$l['manage_smilies'] = "Správa emotikonů";
+$l['manage_smilies_desc'] = "V této části můžeš přidat/upravit a smazat smajlíky.";
+$l['add_smilie'] = "Přidat nového smajlíka";
+$l['add_smilie_desc'] = "Zde můžeš přidat jednoho smajlíka.";
+$l['add_multiple_smilies'] = "Přidání více smajlíků najednou";
+$l['add_multiple_smilies_desc'] = "Zde přidáš více smajlíků najednou.";
+$l['edit_smilie'] = "Upravit smajlíka";
+$l['edit_smilie_desc'] = "Zde můžeš upravit jednoho smajlíka.";
+$l['mass_edit'] = "Upravení více smajliků najednou";
+$l['mass_edit_desc'] = "Zde můžeš snadněji upravit více smajlíků najednou.";
 
-$l['no_smilies'] = "There are no smilies on your forum at this time.";
+$l['no_smilies'] = "Bohužel, ale v tuto chvíli, ve fóru nemůžeš použít žádného smajlíka.";
 
-$l['image'] = "Image";
-$l['name'] = "Name";
-$l['text_replace'] = "Text to Replace";
+$l['image'] = "Obrázek";
+$l['name'] = "Název";
+$l['text_replace'] = "Zobrazení v textu";
 $l['text_replace_desc'] = "This is the text to replace with the smilie (enter one per line).";
-$l['image_path'] = "Image Path";
-$l['image_path_desc'] = "This is the path to the smilie image. If you want to use different smilie images for different themes, please use <strong>{theme}</strong> to represent the image directory of each theme.";
-$l['order'] = "Order";
-$l['display_order'] = "Display Order";
+$l['image_path'] = "Cesta k obrázku";
+$l['image_path_desc'] = "Tady je cesta obrázku smajlíka. Pokud chceš použít jiné obrázky smajlíků pro jiný vzhled fóra, prosím použí <strong>{theme}</strong>, což ti zastoupí složku smajlíků pro každý vzhled.";
+$l['order'] = "Pořadí";
+$l['display_order'] = "Ukázat/zobrazit pořadí";
 $l['display_order_desc'] = "The order on the smilies list that this will appear. This number should not be the same as another smilie's.";
-$l['mass_edit_show_clickable'] = "Show on Clickable?";
-$l['show_clickable'] = "Show on clickable list?";
-$l['show_clickable_desc'] = "Do you want this smilie to show on the clickable smilie list on the post editor?";
-$l['include'] = "Add?";
-$l['path_to_images'] = "Path to Images";
-$l['path_to_images_desc'] = "This is the path to the folder that the images are in.";
-$l['smilie_delete'] = "Delete?";
-$l['save_smilie'] = "Save Smilie";
-$l['save_smilies'] = "Save Smilies";
-$l['show_smilies'] = "Show Smilies";
+$l['mass_edit_show_clickable'] = "Zobrazit ve výběru smajlíků v editoru příspěvků?";
+$l['show_clickable'] = "Zobrazit ve výběru smajlíků v editoru příspěvků?";
+$l['show_clickable_desc'] = "Chceš, abys mohl najít smajlíka při psaní příspěvku?";
+$l['include'] = "Přidat?";
+$l['path_to_images'] = "Cesta k obrazkům";
+$l['path_to_images_desc'] = "Tohle je cesta k složce ve které máš obrázky smajlíků.";
+$l['smilie_delete'] = "Smazat?";
+$l['save_smilie'] = "Uložit smajlíka";
+$l['save_smilies'] = "Uložit smajlíky";
+$l['show_smilies'] = "Ukázat smajlíky";
 $l['reset'] = "Reset";
 
-$l['error_missing_name'] = "You did not enter a name for this smilie.";
-$l['error_missing_text_replacement'] = "You did not enter a text replacement for this smilie.";
-$l['error_missing_path'] = "You did not enter a path for this smilie.";
-$l['error_missing_path_multiple'] = "You did not enter a path.";
-$l['error_missing_order'] = "You did not enter a display order for this smilie.";
-$l['error_duplicate_order'] = "You did not enter a valid display order for this smilie.";
-$l['error_missing_clickable'] = "You did not specify yes or no for the \"Show Clickable\" option.";
-$l['error_no_smilies'] = "There are no smilies in the specified directory, or all smilies in the directory have already been added.";
-$l['error_no_images'] = "There are no images in the specified directory.";
+$l['error_missing_name'] = "Nezadal si název pro tohohle smajlíka.";
+$l['error_missing_text_replacement'] = "Nezadal si <strong>Zobrazení v textu</strong> pro tohohle smajlíka.";
+$l['error_missing_path'] = "Nezadal si cestu pro tohohle smajlíka.";
+$l['error_missing_path_multiple'] = "Nezadal si cestu, kde se nachází smajlíci.";
+$l['error_missing_order'] = "Nevyplňil si pořadí pro smajlíka.";
+$l['error_duplicate_order'] = "Zadal si nesprávnou hodnotu pro zobrazení smajlíka.";
+$l['error_missing_clickable'] = "Nevybral si ano/ne v možnosti pro \"Zobrazení ve výběru smajlíků v editoru příspěvků?\"";
+$l['error_no_smilies'] = "Ve složce, kterou jsi zvolil nejsou žádní smajlíci nebo všechny smajlíky už jsi v minulosti přidal.";
+$l['error_no_images'] = "Ve specifikované složce nejsou žádné obrázky.";
 $l['error_none_included'] = "You did not select any smilies to include.";
-$l['error_invalid_path'] = "You did not enter a valid path.";
+$l['error_invalid_path'] = "Zadal si cestu, která neexistuje.";
 $l['error_invalid_smilie'] = "The specified smilie does not exist.";
 
-$l['success_smilie_added'] = "The smilie has been added successfully.";
-$l['success_multiple_smilies_added'] = "The selected smilies have been added successfully.";
-$l['success_smilie_updated'] = "The smilie has been updated successfully.";
-$l['success_multiple_smilies_updated'] = "The smilies have been updated successfully.";
-$l['success_smilie_deleted'] = "The selected smilie has been deleted successfully.";
-$l['success_mass_edit_updated'] = "The smilies have been updated successfully.";
+$l['success_smilie_added'] = "Smajlík byl úspěšně přidán.";
+$l['success_multiple_smilies_added'] = "Vybraní smajlíci byly úspěšně přidáni.";
+$l['success_smilie_updated'] = "Smajlík byl úspěšně aktualizován.";
+$l['success_multiple_smilies_updated'] = "Smajlíky si úspěšně upravil.";
+$l['success_smilie_deleted'] = "Vybraný smajlík byl vymazán.";
+$l['success_mass_edit_updated'] = "Smajlíci byly úspěšně upraveni.";
 
-$l['confirm_smilie_deletion'] = "Are you sure you wish to delete this smilie?";
+$l['confirm_smilie_deletion'] = "Jsi si jistý, že chceš smazat tohohle smajlíka?";
 
