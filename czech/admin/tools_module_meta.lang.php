f981b212c89ef5212aa83cfe29548462db9dde14
Josef Schlehofer <github@bloodkings.eu>
2017-07-30 00:01:55 +0200

    Přeložení nastavení a údržby

--- czech/admin/tools_module_meta.lang.php
+++ czech/admin/tools_module_meta.lang.php
@@ -5,42 +5,42 @@
  *
  */
 
-$l['tools_and_maintenance'] = "Tools &amp; Maintenance";
+$l['tools_and_maintenance'] = "Nastavení &amp; Údržba";
 
-$l['maintenance'] = "Maintenance";
-$l['logs'] = "Logs";
+$l['maintenance'] = "Údržba";
+$l['logs'] = "Záznamy";
 
 $l['system_health'] = "System Health";
-$l['cache_manager'] = "Cache Manager";
-$l['task_manager'] = "Task Manager";
+$l['cache_manager'] = "Správce mezipaměti";
+$l['task_manager'] = "Naplánované úlohy";
 $l['recount_and_rebuild'] = "Recount &amp; Rebuild";
-$l['view_php_info'] = "View PHP Info";
-$l['database_backups'] = "Database Backups";
-$l['optimize_database'] = "Optimize Database";
-$l['file_verification'] = "File Verification";
+$l['view_php_info'] = "Zobrazit PHP Info";
+$l['database_backups'] = "Zálohy databáze";
+$l['optimize_database'] = "Optimalizování databáze";
+$l['file_verification'] = "Ověření souborů";
 
-$l['administrator_log'] = "Administrator Log";
-$l['moderator_log'] = "Moderator Log";
-$l['user_email_log'] = "User Email Log";
-$l['system_mail_log'] = "System Mail Log";
-$l['user_warning_log'] = "User Warning Log";
-$l['spam_log'] = "Spam Log";
-$l['statistics'] = "Statistics";
+$l['administrator_log'] = "Záznam administrátorů";
+$l['moderator_log'] = "Záznam moderátorů";
+$l['user_email_log'] = "Záznam uživatelských emailů";
+$l['system_mail_log'] = "Záznam systemového emailu";
+$l['user_warning_log'] = "Záznam uživatelských napomenutí";
+$l['spam_log'] = "Záznam anti-spamové protekce";
+$l['statistics'] = "Statistiky";
 
 $l['can_access_system_health'] = "Can access system health?";
-$l['can_manage_cache'] = "Can manage cache?";
-$l['can_manage_tasks'] = "Can manage scheduled tasks?";
-$l['can_manage_db_backup'] = "Can manage backup database?";
-$l['can_optimize_db'] = "Can optimize database?";
+$l['can_manage_cache'] = "Může spravovat mezipaměť?";
+$l['can_manage_tasks'] = "Může spravovat naplánované úlohy?";
+$l['can_manage_db_backup'] = "Může spravovat zálohy databáze?";
+$l['can_optimize_db'] = "Může optimalizovat databázi?";
 $l['can_recount_and_rebuild'] = "Can recount and rebuild?";
-$l['can_manage_admin_logs'] = "Can manage administrator log?";
-$l['can_manage_mod_logs'] = "Can manage moderator log?";
-$l['can_manage_user_mail_log'] = "Can manage user mail Log?";
-$l['can_manage_system_mail_log'] = "Can manage system mail log?";
-$l['can_manage_user_warning_log'] = "Can manage user warning logs?";
-$l['can_manage_spam_log'] = "Can manage spam logs?";
-$l['can_view_php_info'] = "Can view PHP info?";
-$l['can_manage_file_verification'] = "Can manage file verification?";
-$l['can_view_statistics'] = "Can view statistics?";
+$l['can_manage_admin_logs'] = "Může spravovat administrátorské záznamy?";
+$l['can_manage_mod_logs'] = "Může spravovat moderátorské záznamy?";
+$l['can_manage_user_mail_log'] = "Může spravovat záznamy od uživatelských emailů?";
+$l['can_manage_system_mail_log'] = "Může spravovat záznamy od systémových emailů?";
+$l['can_manage_user_warning_log'] = "Může spravovat záznamy od systému uživatelských napomenutí?";
+$l['can_manage_spam_log'] = "Může spravovat záznamy od spamové protekce?";
+$l['can_view_php_info'] = "Může zobrazit informace o PHP serveru?";
+$l['can_manage_file_verification'] = "Může spravovat ověření souborů?";
+$l['can_view_statistics'] = "Může vidět statistiky?";
 
 
