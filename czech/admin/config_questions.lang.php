11e79be4fe4155314ea0c09d7696a29077e3dbf2
Josef Schlehofer <github@bloodkings.eu>
2017-07-29 21:18:29 +0200

    Registrační bezpečnostní otázky

--- czech/admin/config_questions.lang.php
+++ czech/admin/config_questions.lang.php
@@ -5,38 +5,38 @@
  *
  */
 
-$l['security_questions'] = "Security Questions";
-$l['security_questions_desc'] = "Here you can create and manage security questions that will be randomly shown during registration.";
-$l['add_new_question'] = "Add New Question";
-$l['add_new_question_desc'] = "Add a question and its answers for the security questions to be used during registration.";
-$l['edit_question'] = "Edit Question";
-$l['edit_question_desc'] = "Here you edit a security question.";
+$l['security_questions'] = "Registrační otázky";
+$l['security_questions_desc'] = "Zde můžeš vytvářet/upravovat otázky (= ochrana proti spamu), které se ukážou během registrace.";
+$l['add_new_question'] = "Přidat novou otázku";
+$l['add_new_question_desc'] = "Zde můžeš přidat otázky, které se zobrazí při registraci a k ní musíš přidat odpovědi, které požaduješ při vyplnění.";
+$l['edit_question'] = "Upravit otázku";
+$l['edit_question_desc'] = "Zde můžeš upravit registrační otázku.";
 
-$l['options'] = "Options";
-$l['shown'] = "Shown";
-$l['correct'] = "Correct";
-$l['incorrect'] = "Incorrect";
-$l['disable_question'] = "Disable Question";
-$l['enable_question'] = "Enable Question";
-$l['delete_question'] = "Delete Question";
-$l['no_security_questions'] = "There are no security questions on your forum at this time.";
+$l['options'] = "Nastavení";
+$l['shown'] = "Zobrazena";
+$l['correct'] = "Vyplněna správně";
+$l['incorrect'] = "Vyplněna nesprávně";
+$l['disable_question'] = "Zakázat otázku";
+$l['enable_question'] = "Povolit otázku";
+$l['delete_question'] = "Smazat otázku";
+$l['no_security_questions'] = "V tuto chvíli, zde nejsou žádné registrační bezpečnostní otázky.";
 
-$l['question'] = "Question";
-$l['question_desc'] = "Enter the question you want answered during registration.";
-$l['answers'] = "Answer(s)";
-$l['answers_desc'] = "Enter the answer(s) to the question above. Please enter each answer on a separate line.";
+$l['question'] = "Otázka";
+$l['question_desc'] = "Zadej otázku na kterou chceš odpověď při registraci.";
+$l['answers'] = "Odpověd(i)";
+$l['answers_desc'] = "Zadej odpověď(i) k výše uvedené otázce. Prosím zadej každou odpověď na samostatný řádek.";
 $l['active'] = "Active";
-$l['save_question'] = "Save Question";
+$l['save_question'] = "Uložit otázku";
 
 $l['error_invalid_question'] = "You have selected an invalid question.";
-$l['error_missing_question'] = "You did not enter a question for this question";
-$l['error_missing_answer'] = "You did not enter any answers for this question";
+$l['error_missing_question'] = "Nezadal si otázku pro tuhle otázku.";
+$l['error_missing_answer'] = "Nezadal si žádné odpovědi pro tuhle otázku.";
 
-$l['success_question_created'] = "The question has been created successfully.";
-$l['success_question_updated'] = "The question has been updated successfully.";
-$l['success_question_disabled'] = "The question has been disabled successfully.";
-$l['success_question_enabled'] = "The question has been enabled successfully.";
-$l['success_question_deleted'] = "The question has been deleted successfully.";
+$l['success_question_created'] = "Otázka byla úspěšně vytvořena.";
+$l['success_question_updated'] = "Otázka byla úspěšně upravena.";
+$l['success_question_disabled'] = "Otázka byla úspěšně zakázana.";
+$l['success_question_enabled'] = "Otázka byla úspěšně povolena.";
+$l['success_question_deleted'] = "Otázka byla úspěšně vymazána.";
 
-$l['confirm_question_deletion'] = "Are you sure you wish to delete this question?";
+$l['confirm_question_deletion'] = "Jsi si jistý, že chceš smazat tuhle otázku?";
 
