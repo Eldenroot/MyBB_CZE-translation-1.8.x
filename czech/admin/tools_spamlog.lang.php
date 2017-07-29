2b809d7ecdf83c08927ae84b2605378eb66cad0a
Josef Schlehofer <github@bloodkings.eu>
2017-07-29 18:50:48 +0200

    Spamová ochrana

--- czech/admin/tools_spamlog.lang.php
+++ czech/admin/tools_spamlog.lang.php
@@ -5,29 +5,29 @@
  */
 
 
-$l['spam_logs']       = 'Spam Logs';
-$l['spam_logs_desc']  = 'This section allows you to view a history of users blocked by the spam filters.';
-$l['prune_spam_logs']       = 'Prune Spam Logs';
+$l['spam_logs']       = 'Ochrana proti spamu';
+$l['spam_logs_desc']  = 'V této části můžeš vidět historii uživatelů, které zablokoval spam filter';
+$l['prune_spam_logs']       = 'Smazat historii spamu';
 $l['prune_spam_logs_desc']  = 'Here you can prune the spam logs matching a specified criteria.';
-$l['spam_username']   = 'Username';
-$l['spam_email']      = 'Email Address';
-$l['spam_ip']         = 'IP Address';
-$l['spam_date']       = 'Date';
-$l['spam_confidence'] = 'Confidence';
-$l['no_spam_logs'] = 'No users have been blocked by the spam filters yet.';
-$l['success_pruned_spam_logs'] = 'The spam logs have been pruned successfully.';
-$l['note_logs_locked'] = 'For security reasons, logs less than 24 hours old cannot be pruned.';
-$l['all_usernames'] = 'All usernames';
-$l['all_emails'] = ' All Email Addresses';
-$l['date_range'] = "Date range:";
-$l['days'] = "days";
-$l['filter_spam_logs'] = 'Filter Spam Logs';
-$l['asc'] = "Ascending";
-$l['desc'] = "Descending";
-$l['search_ip_on_sfs'] = "Search this IP on Stop Forum Spam";
-$l['search'] = "Search";
+$l['spam_username']   = 'Přezdívka';
+$l['spam_email']      = 'Email';
+$l['spam_ip']         = 'IP address';
+$l['spam_date']       = 'Datum';
+$l['spam_confidence'] = 'Důkaz';
+$l['no_spam_logs'] = 'Prozatím spamová ochrana nezabanovala žádného uživatele.';
+$l['success_pruned_spam_logs'] = 'Historie spamů byla úspěšně vymazána.';
+$l['note_logs_locked'] = 'Z bezpečnostních důvodu nemůžeš smazat záznamy, které nejsou starší než 24 hodin.';
+$l['all_usernames'] = 'Všechny přezdívky';
+$l['all_emails'] = ' Všechny emailové adresy';
+$l['date_range'] = "Od kdy - do kdy:";
+$l['days'] = "dny";
+$l['filter_spam_logs'] = 'Hledat v záznamech spamu';
+$l['asc'] = "Vzestupně";
+$l['desc'] = "Klesající";
+$l['search_ip_on_sfs'] = "Najít tuto IP adresa v databázi: <a href='https://www.stopforumspam.com/' target='_blank'>Stop Forum Spam</a>";
+$l['search'] = "Hledat";
 
 $l['in'] = "in";
 $l['order'] = "order";
-$l['sort_by'] = "Sort By";
-$l['results_per_page'] = "Results Per Page";
+$l['sort_by'] = "Seřazeno podle";
+$l['results_per_page'] = "Výsledky na stránku";
