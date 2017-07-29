8e345bbd0df8ee54f2139c358ec75a7cf3c2b41f
Josef Schlehofer <github@bloodkings.eu>
2017-07-30 00:38:36 +0200

    Překlad historie napomenutí

--- czech/admin/config_badwords.lang.php
+++ czech/admin/config_badwords.lang.php
@@ -5,31 +5,31 @@
  *
  */
 
-$l['bad_words'] = "Word Filters";
-$l['edit_bad_word'] = "Edit Filter";
-$l['edit_bad_word_desc'] = "Here you can edit the filter and its replacement.";
+$l['bad_words'] = "Slovní filtr";
+$l['edit_bad_word'] = "Upravit filtr";
+$l['edit_bad_word_desc'] = "Zde můžeš upravit filtr a upravit za co dané slovo nahradíš.";
 $l['bad_word_filters'] = "Word Filters";
-$l['bad_word_filters_desc'] = "This feature allows you to manage a listing of words or phrases which are automatically replaced in posts on your forum. It is useful for replacing swear words and such.";
+$l['bad_word_filters_desc'] = "Tato funkce ti umožní spravovat seznam slov nebo frází, které mají být automaticky nahrazeny v příspěvcích uživatelů.  Je to velmi užitečné při nahrazení sprostých slov, apod.";
 
-$l['bad_word'] = "Word";
+$l['bad_word'] = "Slovo";
 $l['bad_word_desc'] = "Enter the word which you wish to be filtered. The '*' character represents any single character (a-zA-Z0-9_).";
 $l['bad_word_max'] = "A filtered word can't be longer than 100 characters.";
-$l['replacement'] = "Replacement";
+$l['replacement'] = "Nahrazeno";
 $l['replacement_desc'] = "Enter the string which will replace the filtered word (If this is blank, asterisks will be shown)";
-$l['replacement_word_max'] = "A replacement word can't be longer than 100 characters.";
-$l['error_replacement_word_invalid'] = "A replacement word cannot be equivalent to it's word filter.";
+$l['replacement_word_max'] = "Nahrazené slovo nemůže být delší než 100 znaků.";
+$l['error_replacement_word_invalid'] = "Slovo, které chceš nahradit nemůže být zároveň tím samým slovem, které nemohou uživatelé použít.";
 
-$l['save_bad_word'] = "Save Filter";
-$l['no_bad_words'] = "There are no word filters currently set at this time.";
-$l['add_bad_word'] = "Add a Filter";
+$l['save_bad_word'] = "Uložit filtr";
+$l['no_bad_words'] = "V tuto chvíli, zde nemáš žádná slova, která mají být nahrazena.";
+$l['add_bad_word'] = "Přidat filtr";
 
-$l['error_missing_bad_word'] = "You did not enter a word filter.";
-$l['error_invalid_bid'] = "The specified filter does not exist.";
-$l['error_bad_word_filtered'] = "The filter you entered already exists.";
+$l['error_missing_bad_word'] = "Nezadal si slovo, které má být nahrazeno..";
+$l['error_invalid_bid'] = "Zvolený filtr neexistuje.";
+$l['error_bad_word_filtered'] = "Filtr, který si zadal, již existuje.";
 
-$l['success_added_bad_word'] = "The filter has been added successfully.";
-$l['success_deleted_bad_word'] = "The filter has been deleted successfully.";
-$l['success_updated_bad_word'] = "The filter has been updated successfully.";
+$l['success_added_bad_word'] = "Filtr byl úspěšně přidán.";
+$l['success_deleted_bad_word'] = "Filtr byl úspěšně vymazán.";
+$l['success_updated_bad_word'] = "Filtr byl úspěšně upraven.";
 
-$l['confirm_bad_word_deletion'] = "Are you sure you wish to delete this filter?";
+$l['confirm_bad_word_deletion'] = "Jsi si jistý, že chceš smazat tento filtr?";
 
