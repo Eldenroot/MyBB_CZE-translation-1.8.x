464bc8a79bf554262e2559550cccbd9772dceee0
Josef Schlehofer <github@bloodkings.eu>
2017-07-31 21:14:32 +0200

    Překlad: Administrace: Uživatelé (in progress - status: 60%)

--- czech/admin/user_users.lang.php
+++ czech/admin/user_users.lang.php
@@ -15,9 +15,9 @@ $l['find_users_desc'] = "Here you can search for users of your forum. The fewer 
 $l['create_user'] = "Vytvořit nového uživatele";
 $l['create_user_desc'] = "Zde můžeš vytvořit nového uživatele fóra";
 $l['merge_users'] = "Sloučit uživatele";
-$l['merge_users_desc'] = "Here you can merge two user accounts in to one. The \"Source Account\" will  be merged in to the \"Destination Account\" leaving <strong>only</strong> the destination account. The source accounts posts, threads, private messages, calendar events, post count and buddy list will be merged in to the destination account.<br /><span style=\"font-size: 15px;\">Please be aware that this process cannot be undone.</span>";
+$l['merge_users_desc'] = "Zde můžeš sloužit dva uživatelské účty do jednoho. <br /> Ze <strong>Sekundárního účtu</strong> se přesunou příspěvky, vlákna, soukromé zprávy, události v kalendáři, počet příspěvků a také seznam (ne)přátel do <strong>Primárního účtu</strong><br /><span style=\"font-size: 15px;\">Protože bude Vedlejší účet smazán je tento proces je nevratný!</span>";
 $l['edit_user'] = "Upravit uživatele";
-$l['edit_user_desc'] = "Here you can edit this users profile, settings, and signature; see general statistics; and visit other pages for further information relating to this user.";
+$l['edit_user_desc'] = "Zde můžeš upravit uživatelovi profil a jeho nastavení, podpis. Také zde můžeš vidět základní statistiky uživatele. and visit other pages for further information relating to this user.";
 $l['show_referrers'] = "Show Referrers";
 $l['show_referrers_desc'] = "The results to your search criteria are shown below. You can view the results in either a table view or business card view.";
 $l['show_ip_addresses'] = "Show IP Addresses";
@@ -25,10 +25,10 @@ $l['show_ip_addresses_desc'] = "The registration IP address and the post IPs for
 $l['manage_users'] = "Manage Users";
 $l['manage_users_desc'] = "Mass-managing users makes it a lot easier to do common tasks.";
 $l['inline_edit'] = "Inline User Moderation:";
-$l['inline_activate'] = "Aktivní uživatel(é)";
-$l['inline_ban'] = "Zabanován(í) uživatel(é)";
+$l['inline_activate'] = "Aktivní uživatel(e)";
+$l['inline_ban'] = "Zabanován(í) uživatel(e)";
 $l['inline_usergroup'] = "Change Users' Usergroup";
-$l['inline_delete'] = "Smazat uživatel(é)";
+$l['inline_delete'] = "Smazat uživatel(e)";
 $l['inline_prune'] = "Prune/Delete Users' Posts";
 $l['inline_activated'] = "{1} user(s) were successfully activated.";
 $l['inline_activated_more'] = "<small>{1} user(s) you selected were already activated.</small>";
@@ -86,14 +86,14 @@ $l['option_4'] = "Více než rok staré";
 $l['option_5'] = "Více než rok a půl staré.d";
 $l['option_6'] = "Více než 2 roky staré";
 
-$l['error_avatartoobig'] = "Sorry, but we cannot change your avatar as the new avatar you specified is too big. The maximum dimensions are {1}x{2} (width x height)";
-$l['error_invalidavatarurl'] = "The URL you entered for your avatar does not appear to be valid. Please ensure you enter a valid URL.";
+$l['error_avatartoobig'] = "Promiň, ale nemůžeme změnit tvého avatara, protože avatar, kterého si vložil je přílíš velký. Maximální rozměry jsou: {1}x{2} (šířka x výška)";
+$l['error_invalidavatarurl'] = "Odkaz, který si zadal pro tvého avatara je neplatný. Podívej se, zda si vložil správný odkaz URL.";
 $l['error_remote_avatar_not_allowed'] = "Remote avatar URLs have been disabled by the forum administrator.";
-$l['error_invalid_user'] = "You have selected an invalid user.";
-$l['error_no_perms_super_admin'] = "You do not have permission to edit this user because you are not a super administrator.";
-$l['error_invalid_user_source'] = "The source account username you entered does not exist";
-$l['error_invalid_user_destination'] = "The destination account username you entered does not exist";
-$l['error_cannot_merge_same_account'] = "The source and destination accounts must be different";
+$l['error_invalid_user'] = "Vybral si neplatného uživatele.";
+$l['error_no_perms_super_admin'] = "Nemáš právo upravit tohohle uživatele, protože nejsi super administrátor.";
+$l['error_invalid_user_source'] = "Sekundární účet, který si zadal, neexistuje.";
+$l['error_invalid_user_destination'] = "Primární účet, který si zadal, neexistuje.";
+$l['error_cannot_merge_same_account'] = "Nemůžeš sloučet stejný účet.";
 $l['error_no_users_found'] = "No users were found matching the specified search criteria. Please modify your search criteria and try again.";
 $l['error_invalid_admin_view'] = "You selected an invalid administration view.";
 $l['error_missing_view_title'] = "You did not enter a title for this view.";
@@ -104,16 +104,16 @@ $l['error_invalid_view_sortorder'] = "You have selected an invalid sort order";
 $l['error_invalid_view_delete'] = "You selected an invalid administration view to delete";
 $l['error_cannot_delete_view'] = "You must have at least 1 administration view.";
 $l['error_inline_no_users_selected'] = "Sorry, but you did not select any users. Please select some users and try again.";
-$l['error_cannot_delete_user'] = "This user cannot be deleted.";
+$l['error_cannot_delete_user'] = "Tento uživatel nemůže být vymazán.";
 $l['error_no_referred_users'] = "The selected user does not have any referred users.";
 
-$l['user_deletion_confirmation'] = "Are you sure you wish to delete this user?";
+$l['user_deletion_confirmation'] = "Jsi si doopravdy jistý, že chceš smazat tohoto uživatele?";
 
 $l['success_coppa_activated'] = "The selected COPPA user has been activated successfully.";
 $l['success_activated'] = "The selected user has been activated successfully.";
-$l['success_user_created'] = "The user has been created successfully.";
-$l['success_user_updated'] = "The selected user has been updated successfully.";
-$l['success_user_deleted'] = "The selected user has been deleted successfully.";
+$l['success_user_created'] = "Účet byl úspěšně vytvořen.";
+$l['success_user_updated'] = "Zvolený účet byl úspěšně aktualizován.";
+$l['success_user_deleted'] = "Zvolený účet byl vymazán.";
 $l['success_merged'] = "has successfully been merged in to";
 $l['succuss_view_set_as_default'] = "The selected administration view has been set as your default successfully";
 $l['success_view_created'] = "The administration view has been created successfully.";
@@ -122,7 +122,7 @@ $l['success_view_deleted'] = "The selected administration view has been deleted 
 
 $l['confirm_view_deletion'] = "Are you sure you want to delete the selected view?";
 
-$l['warning_coppa_user'] = "<p class=\"alert\"><strong>Warning: </strong> This user is awaiting COPPA validation. <a href=\"index.php?module=user-users&amp;action=activate_user&amp;uid={1}\">Activate Account</a></p>";
+$l['warning_coppa_user'] = "<p class=\"alert\"><strong>Upozornění: </strong> This user is awaiting COPPA validation. <a href=\"index.php?module=user-users&amp;action=activate_user&amp;uid={1}\">Activate Account</a></p>";
 
 $l['required_profile_info'] = "Požadované informace";
 $l['password'] = "Heslo";
@@ -135,7 +135,7 @@ $l['additional_user_groups_desc'] = "Use CTRL to select multiple groups";
 $l['display_user_group'] = "Display User Group";
 $l['save_user'] = "Uložit uživatele";
 
-$l['overview'] = "Overview";
+$l['overview'] = "Přehled";
 $l['profile'] = "Profil";
 $l['account_settings'] = "Nastavení účtu";
 $l['signature'] = "Podpis";
@@ -148,43 +148,43 @@ $l['posts'] = "Příspěvky";
 $l['age'] = "Věk";
 $l['posts_per_day'] = "Příspěvky za den";
 $l['percent_of_total_posts'] = "Procento všech příspěvků";
-$l['user_overview'] = "User Overview";
+$l['user_overview'] = "Přehled uživatele";
 
 $l['new_password'] = "Nové heslo";
-$l['new_password_desc'] = "Only required if changing";
+$l['new_password_desc'] = "Vyžadováno pouze při změně";
 $l['confirm_new_password'] = "Znovu zadej nové heslo";
 
 $l['optional_profile_info'] = "Volitelné uživatelské informace";
 $l['custom_user_title'] = "Custom User Title";
 $l['custom_user_title_desc'] = "If empty, the group user title will be used";
 $l['website'] = "Stránka";
-$l['icq_number'] = "ICQ";
-$l['aim_handle'] = "AIM Handle";
-$l['yahoo_messanger_handle'] = "Yahoo! Messenger Handle";
-$l['skype_handle'] = "Skype";
-$l['google_handle'] = "Google Hangouts Handle";
+$l['icq_number'] = "ICQ číslo";
+$l['aim_handle'] = "AIM jméno";
+$l['yahoo_messanger_handle'] = "Yahoo ID";
+$l['skype_handle'] = "Skype přezdívka";
+$l['google_handle'] = "Google Hangouts ID";
 $l['birthday'] = "Datum narození";
 
-$l['away_information'] = "Away Information";
-$l['away_status'] = "Away Status:";
-$l['away_status_desc'] = "Allows you to leave an away message if you are going away for a while.";
-$l['im_away'] = "I'm Away";
+$l['away_information'] = "Informace nepřítomnosti";
+$l['away_status'] = "Status nepřímnosti:";
+$l['away_status_desc'] = "Umožnuje zanechat zprávu v době nepřímnosti.";
+$l['im_away'] = "Nepřítomem";
 $l['im_here'] = "I'm Here";
-$l['away_reason'] = "Away Reason:";
-$l['away_reason_desc'] = "Allows you to enter a small description of why you are away  (max 200 characters).";
-$l['return_date'] = "Return Date:";
-$l['return_date_desc'] = "If you know when you will be back, you can enter your return date here.";
+$l['away_reason'] = "Důvod nepřítomnosti";
+$l['away_reason_desc'] = "Umožňuje zanechat krátkou zprávu, proč budeš nepřítomný (pouze 200 znaků).";
+$l['return_date'] = "Datum, kdy se vracíš:";
+$l['return_date_desc'] = "Pokud víš, kdy se vrátíš, můžeš jsem zadat datum návratu.";
 $l['error_acp_return_date_past'] = "You cannot return in the past!";
 
 $l['hide_from_whos_online'] = "Hide from the Who's Online list";
-$l['login_cookies_privacy'] = "Login, Cookies &amp; Privacy";
-$l['recieve_admin_emails'] = "Receive emails from administrators";
-$l['hide_email_from_others'] = "Hide email address from other members";
-$l['recieve_pms_from_others'] = "Receive private messages from other users";
-$l['recieve_pms_from_buddy'] = "Only receive private messages from buddy list (this setting has no effect unless there is at least one buddy on the list)";
+$l['login_cookies_privacy'] = "Přihlášení, soukromí a cookies (sušenky)";
+$l['recieve_admin_emails'] = "Dostávat emaily od administrátorů";
+$l['hide_email_from_others'] = "Schovat emailovou adresu pro ostatní uživatele";
+$l['recieve_pms_from_others'] = "Přijímat soukromé zprávy od ostatních uživatelů";
+$l['recieve_pms_from_buddy'] = "Dostávat soukromé zprávy pouze od přátel (this setting has no effect unless there is at least one buddy on the list)";
 $l['alert_new_pms'] = "Alert with notice when new private message is received";
 $l['email_notify_new_pms'] = "Notify by email when new private message is received";
-$l['buddy_requests_pm'] = "Receive PM notifications for new buddy requests";
+$l['buddy_requests_pm'] = "Dostat oznámení soukromou zprávou pro novou žádost o přátelství.";
 $l['buddy_requests_auto'] = "Automatically accept buddy requests (if the above checkbox is ticked, a PM is sent informing of the new buddy connection)";
 $l['default_thread_subscription_mode'] = "Default thread subscription mode";
 $l['do_not_subscribe'] = "Do not subscribe";
@@ -192,24 +192,24 @@ $l['no_email_notification'] = "No email notification";
 $l['instant_email_notification'] = "Instant email notification";
 $l['messaging_and_notification'] = "Messaging &amp; Notification";
 $l['use_default'] = "Použít vychození nastavení";
-$l['date_format'] = "Date Format";
-$l['time_format'] = "Time Format";
-$l['time_zone'] = "Time Zone";
+$l['date_format'] = "Formát data";
+$l['time_format'] = "Formát času";
+$l['time_zone'] = "Časové pásmo";
 $l['daylight_savings_time_correction'] = "Daylight Saving Time correction";
 $l['automatically_detect'] = "Automatically detect DST settings";
 $l['always_use_dst_correction'] = "Always use DST correction";
 $l['never_use_dst_correction'] = "Never use DST correction";
 $l['date_and_time_options'] = "Date &amp; Time Options";
-$l['show_threads_last_day'] = "Show threads from the last day";
-$l['show_threads_last_5_days'] = "Show threads from the last 5 days";
-$l['show_threads_last_10_days'] = "Show threads from the last 10 days";
-$l['show_threads_last_20_days'] = "Show threads from the last 20 days";
-$l['show_threads_last_50_days'] = "Show threads from the last 50 days";
-$l['show_threads_last_75_days'] = "Show threads from the last 75 days";
-$l['show_threads_last_100_days'] = "Show threads from the last 100 days";
-$l['show_threads_last_year'] = "Show threads from the last year";
-$l['show_all_threads'] = "Show all threads";
-$l['threads_per_page'] = "Threads Per Page";
+$l['show_threads_last_day'] = "Zobrait vlákna za poslední den";
+$l['show_threads_last_5_days'] = "Zobrazit vlákna za posledních 5 dní";
+$l['show_threads_last_10_days'] = "Zobrazit vlákna za posledních 10 dní";
+$l['show_threads_last_20_days'] = "Zobrazit vlákna za posledních 20 dní";
+$l['show_threads_last_50_days'] = "Zobrazit vlákna za posledních 50 dní";
+$l['show_threads_last_75_days'] = "Zobrazit vlákna za posledních 75 dní";
+$l['show_threads_last_100_days'] = "Zobrazit vlákna za posledních 100 dní";
+$l['show_threads_last_year'] = "Zobrait vlákna za poslední rok";
+$l['show_all_threads'] = "Zobrazit všechna vlákna";
+$l['threads_per_page'] = "Vlákna na stránku";
 $l['default_thread_age_view'] = "Default Thread Age View";
 $l['forum_display_options'] = "Forum Display Options";
 $l['show_classic_postbit'] = "Display posts in classic mode";
@@ -226,12 +226,12 @@ $l['thread_view_options'] = "Thread View Options";
 $l['show_redirect'] = "Show friendly redirection pages";
 $l['show_code_buttons'] = "Show MyCode formatting options on posting pages";
 $l['source_editor'] = "Put the editor in source mode by default";
-$l['theme'] = "Theme";
-$l['board_language'] = "Board Language";
-$l['other_options'] = "Other Options";
+$l['theme'] = "Vzhled";
+$l['board_language'] = "Jazyk fóra";
+$l['other_options'] = "Ostatní nastavení";
 $l['signature_desc'] = "Formatting options: MyCode is {1}, smilies are {2}, IMG code is {3}, HTML is {4}";
-$l['enable_sig_in_all_posts'] = "Enable signature in all posts";
-$l['disable_sig_in_all_posts'] = "Disable signature in all posts";
+$l['enable_sig_in_all_posts'] = "Povolit podpis ve všech příspěvcích.";
+$l['disable_sig_in_all_posts'] = "Zakázat podpis ve všech příspěvcích.";
 $l['do_nothing'] = "Do not change signature preferences";
 $l['signature_preferences'] = "Signature Preferences";
 $l['suspend_sig'] = "Suspend Signature";
@@ -266,24 +266,24 @@ $l['expire_days'] = "day(s)";
 $l['expire_weeks'] = "week(s)";
 $l['expire_months'] = "month(s)";
 $l['expire_never'] = "Never";
-$l['expire_permanent'] = "Permanent";
+$l['expire_permanent'] = "Doživotní";
 
 $l['username'] = "Uživatel";
 $l['email'] = "Email";
-$l['primary_group'] = "Primary Group";
-$l['additional_groups'] = "Additional Groups";
+$l['primary_group'] = "Hlavní skupina";
+$l['additional_groups'] = "Další skupiny";
 $l['registered'] = "Registrovaný";
 $l['last_active'] = "Naposledy aktivní";
-$l['post_count'] = "Post Count";
-$l['thread_count'] = "Thread Count";
-$l['reputation'] = "Reputation";
-$l['warning_level'] = "Warning Level";
+$l['post_count'] = "Počet příspěvku";
+$l['thread_count'] = "Počet vytvořených vláken";
+$l['reputation'] = "Reputace";
+$l['warning_level'] = "Úroveň varování";
 $l['registration_ip'] = "IP adresa se kterou se registroval";
 $l['last_known_ip'] = "Poslední jeho znamá IP adresa";
-$l['registration_date'] = "Registration Date";
-$l['info_on_ip'] = "Information on this IP address";
+$l['registration_date'] = "Zaregistrován dne";
+$l['info_on_ip'] = "Informace k této IP addrese";
 
-$l['current_avatar'] = "Current Avatar";
+$l['current_avatar'] = "Současný avatar";
 $l['user_current_using_uploaded_avatar'] = "This user is currently using an uploaded avatar.";
 $l['user_currently_using_remote_avatar'] = "This user is currently using a remotely linked avatar.";
 $l['max_dimensions_are'] = "The maximum dimensions for avatars are";
@@ -305,11 +305,11 @@ $l['show_users_posted_with_ip'] = "Najdi uživatele, kteří psali ve fóru s IP
 $l['ban_ip'] = "Ban IP";
 $l['ip_address_for'] = "IP adresy zabanovány pro";
 
-$l['source_account'] = "Source Account";
-$l['source_account_desc'] = "This is the account that will be merged in to the destination account. It will be removed after this process.";
-$l['destination_account'] = "Destination Account";
-$l['destination_account_desc'] = "This is the account that the source account will be merged in to. It will remain after this process.";
-$l['merge_user_accounts'] = "Merge User Accounts";
+$l['source_account'] = "Sekundární účet";
+$l['source_account_desc'] = "This is the account that will be merged in to the Hlavní účet. It will be removed after this process.";
+$l['destination_account'] = "Primární účet";
+$l['destination_account_desc'] = "This is the account that the Vedlejší will be merged in to. It will remain after this process.";
+$l['merge_user_accounts'] = "Sloučit!";
 
 $l['display_options'] = "Display Options";
 $l['ascending'] = "Ascending";
@@ -325,9 +325,9 @@ $l['manage_views'] = "Manage Views";
 $l['none'] = "None";
 $l['search'] = "Search";
 
-$l['view_profile'] = "View Profile";
-$l['edit_profile_and_settings'] = "Edit Profile &amp; Settings";
-$l['ban_user'] = "Ban User";
+$l['view_profile'] = "Ukázat profil";
+$l['edit_profile_and_settings'] = "Upravit profil &amp; nastavení";
+$l['ban_user'] = "Zabanovat uživatele";
 $l['approve_coppa_user'] = "Activate COPPA User";
 $l['approve_user'] = "Activate User";
 $l['delete_user'] = "Delete User";
@@ -359,8 +359,8 @@ $l['posted_with_ip'] = "Has posted with the IP address";
 $l['custom_profile_fields_match'] = "Where custom profile fields match...";
 $l['is_not_blank'] = " is not empty";
 $l['or'] = "or";
-$l['reg_in_x_days'] = "Registered in the last";
-$l['days'] = "days";
+$l['reg_in_x_days'] = "Zaregistrován v posledních";
+$l['days'] = "dnech";
 
 $l['view'] = "View";
 $l['create_new_view'] = "Create New View";
@@ -392,19 +392,19 @@ $l['title'] = "Title";
 
 $l['view_title_1'] = "All Users";
 
-$l['emailsubject_activateaccount'] = "Account Activation at {1}";
+$l['emailsubject_activateaccount'] = "Aktivace účtu na {1}";
 $l['email_adminactivateaccount'] = "{1},
 
-The administrator has activated your forum account on {2}.
+Administrátor právě teď aktivoval tvůj účet na {2}.
 
-To proceed, please go to
+Pokračuj, prosím na
 
 {3}
 
-You will be able to login with the credentials you registered with.
+Přihlasíš se pomocí údajů, které si zadal při registraci.
 
-Thank you,
-{2} Staff";
+S pozdravem,
+{2} Vedení";
 
 $l['ipaddress_misc_info'] = "Misc. Information for '{1}'";
 $l['ipaddress_host_name'] = "Host Name";
