428068c79324b5feccbfbdec604c506599f1998a
Josef Schlehofer <github@bloodkings.eu>
2018-06-25 22:24:29 +0200

    showteam.lang.php: dopřeložení nové funkce pro zobrazení vedení fóra

--- czech/showteam.lang.php
+++ czech/showteam.lang.php
@@ -10,15 +10,17 @@ $l['forum_team'] = "Tým fóra";
 $l['moderators'] = "Moderátoři";
 $l['username'] = "Uživatelské jméno";
 $l['lastvisit'] = "Poslední návštěva";
-$l['e-mail'] = "E-mail";
+$l['email'] = "E-mail";
 $l['pm'] = "SZ";
 $l['mod_forums'] = "Fórum (fóra)";
+$l['mod_groups'] = "Skupina (y)";
 $l['online'] = "Online";
 $l['offline'] = "Offline";
 
-$l['group_leaders'] = "Vedoucí";
+$l['group_leaders'] = "Vedoucí skupiny";
 $l['group_members'] = "Členové";
 
 $l['no_members'] = "V této skupině nejsou žádní členové";
 
+$l['showteam_disabled'] = "Administrátor zakázal možnost zobrazení týmu fóra.";
 $l['error_noteamstoshow'] = "Fórum nemá žádné správce";
