e45d5a959a864699c373da5ec42811bb29224cff
Josef Schlehofer <github@bloodkings.eu>
2018-06-25 22:23:48 +0200

    Nahlásit příspěvek nikoliv jej ohlásit

--- czech/report.lang.php
+++ czech/report.lang.php
@@ -1,19 +1,21 @@
 <?php
 /**
- * MyBB 1.8 Czech Language Pack
- * Copyright 2014 MyBB Group, All Rights Reserved
+ * MyBB 1.8.x Official Czech Language Pack
+ * Copyright © 2014-2018 MyBB Group 
+ * All Rights Reserved 
  *
+ * Issues and pull request belongs to the https://github.com/Eldenroot/MyBB_CZE-translation-1.8.x/
  */
 
-$l['report_reason'] = "Důvod ohlášení:";
-$l['report_to_mod'] = "Ohlásit příspěvek moderátorovi";
+$l['report_reason'] = "Důvod nahlášení:";
+$l['report_to_mod'] = "Nahlásit příspěvek moderátorovi";
 $l['close_window'] = "Zavřít okno";
 
 // Content types
-$l['report_content'] = "Ohlásit příspěvek";
-$l['report_reason_post'] = "Ohlásit příspěvek";
-$l['report_reason_profile'] = "Ohlásit profil";
-$l['report_reason_reputation'] = "Ohlásit hodnocení";
+$l['report_content'] = "Nahlásit příspěvek";
+$l['report_reason_post'] = "Nahlásit příspěvek";
+$l['report_reason_profile'] = "Nahlásit profil";
+$l['report_reason_reputation'] = "Nahlásit hodnocení";
 
 // Content reasons
 $l['report_reason_rules'] = "Porušení pravidel fóra";
@@ -22,11 +24,11 @@ $l['report_reason_spam'] = "Spam";
 $l['report_reason_wrong'] = "Špatně zařazené fórum";
 $l['report_reason_other'] = "Ostatní";
 
-$l['success_report_voted'] = "Děkujeme za ohlášený příspěvek.<br />Moderátoři se jím budou brzy zabývat.";
+$l['success_report_voted'] = "Děkujeme, že udržujete pořádek na fóru.<br />Moderátoři se tím budou brzy zabývat.";
 
 $l['error_report_length'] = "Uveďte prosím důvod vašeho ohlášení.";
-$l['error_invalid_report'] = "Tento příspěvek buď neexistuje, nebo jej nelze ohlásit.";
+$l['error_invalid_report'] = "Tohle {1} buď neexistuje nebo jej nelze nahlásit.";
 $l['error_invalid_report_reason'] = "Uvedený důvod je neplatný.";
 $l['error_comment_required'] = "Je vyžadován komentář pro zvolený důvod.";
-$l['error_report_duplicate'] = "Tento příspěvek již byl ohlášen jiným členem fóra.<br />Mužete jej také níže ohlásit.";
+$l['error_report_duplicate'] = "Tento příspěvek již byl ohlášen jiným členem fóra.<br />Mužete jej také níže nahlásit.";
 $l['report_reason_other_description'] = "Uveďte, prosím, více informací proč ohlašujete tento příspěvěk.";
