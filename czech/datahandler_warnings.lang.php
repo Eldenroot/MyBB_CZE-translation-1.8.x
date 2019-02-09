e24c916563631d2fa2f59987c0a5a6e6c53782b8
Josef Schlehofer <pepe.schlehofer@gmail.com>
2019-02-10 00:31:45 +0100

    datahandler_warnings.lang.php: udělit vs dát varování

--- czech/datahandler_warnings.lang.php
+++ czech/datahandler_warnings.lang.php
@@ -7,14 +7,14 @@
  * Issues and pull request belongs to the https://github.com/Eldenroot/MyBB_CZE-translation-1.8.x/
  */
 
-$l['warnings_reached_max_warnings_day'] = 'Dosáhli jste maximálního povoleného počtu varování, které můžete denně udělit.<br /><br />Maximální povolený počet je {1}."';
+$l['warnings_reached_max_warnings_day'] = 'Dosáhl(a) jste maximálního povoleného počtu varování, které můžete denně udělit.<br /><br />Maximální povolený počet je {1}."';
 $l['warnings_error_invalid_user'] = "Vybraný uživatel neexistuje.";
 $l['warnings_error_invalid_post'] = "Vybraný příspěvek neexistuje.";
 $l['warnings_error_cannot_warn_self'] = "Nemůžete přidat vlastní úroveň varování.";
 $l['warnings_error_user_reached_max_warning'] = "Tento uživatel nemůže být varován, protože již dosáhl maximální úrovně varování.";
 $l['warnings_error_no_note'] = "Nezadal(a) jste k tomuto varování žádnou poznámku.";
 $l['warnings_error_invalid_type'] = "Vybral(a) jste neplatný typ varování.";
-$l['warnings_error_cant_custom_warn'] = "Nemáte oprávnění dávat vlastní varování uživatelům.";
+$l['warnings_error_cant_custom_warn'] = "Nemáte oprávnění udělit vlastní varování uživatelům.";
 $l['warnings_error_no_custom_reason'] = "Nezadal(a) jste důvod pro Vaše varování.";
 $l['warnings_error_invalid_custom_points'] = "Nezadal(a) jste platný počet bodů pro tuto úroveň varování. Musíte zadat číselnou hodnotu větší než 0 a ne větší než {1}.";
 $l['warnings_error_invalid_expires_period'] = "Zadal(a) jste neplatné vypršení platnosti varování.";
